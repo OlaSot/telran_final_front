a335a6c224e85de08a6e9da3b685fbca8603d12f
Olha Sotova <sotovaola21@gmail.com>
2023-11-12 16:31:36 +0100

    image

--- src/components/NavMenu/index.jsx
+++ src/components/NavMenu/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import s from "./index.module.css";
-import logo from '/logo.png'
+import logo from './logo.png'
 
 import Btn from "../Btn";
 
